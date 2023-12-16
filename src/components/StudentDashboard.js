e383545b1446fe47822d24d7f27f44cdf35c8b9e
Aniket Kumar <kraniket123654@gmail.com>
2023-12-16 10:54:10 +0530

    updated studentDashboard

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -1,34 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import courseModel from '../courseModel';
+import courseModel from '../courseModel'; // Update the path accordingly
 
 const StudentDashboard = () => {
   const [enrolledCourses, setEnrolledCourses] = useState([]);
+  const [allCourses, setAllCourses] = useState([]);
 
   useEffect(() => {
-    // Fetch enrolled courses from the backend
-    // Implement the logic to get enrolled courses based on user
-    // axios.get('your_enrollment_api').then((response) => {
-    //   setEnrolledCourses(response.data);
+    // Simulating fetching all courses from the backend
+    // In a real application, you'd fetch this data from your server
+    // axios.get('your_courses_api').then((response) => {
+    //   setAllCourses(response.data);
     // });
-    setEnrolledCourses(courseModel.students); // Replace with actual data
+
+    // Simulated data (replace with actual data fetching logic)
+    setAllCourses(courseModel);
   }, []);
 
-  const markAsCompleted = (courseId) => {
-    // Implement logic to mark a course as completed
-    // axios.post('mark_as_completed_api', { courseId });
+  const enrollInCourse = (courseId) => {
+    // Simulating enrolling in a course
+    // In a real application, you'd make a request to your server to update enrollment status
+    // axios.post('your_enrollment_api', { courseId });
+
+    // Simulated logic (replace with actual logic)
+    const updatedCourses = allCourses.map((course) => {
+      if (course.id === courseId) {
+        return { ...course, enrollmentStatus: 'Enrolled' };
+      }
+      return course;
+    });
+
+    setEnrolledCourses((prevEnrolledCourses) => [
+      ...prevEnrolledCourses,
+      ...updatedCourses.filter((course) => course.id === courseId),
+    ]);
   };
 
   return (
     <div>
       <h1>Student Dashboard</h1>
+      <h2>Enrolled Courses</h2>
       <ul>
         {enrolledCourses.map((course) => (
           <li key={course.id}>
             <h3>{course.name}</h3>
             <p>Instructor: {course.instructor}</p>
-            {/* Display other course details based on the assignment */}
-            <button onClick={() => markAsCompleted(course.id)}>
-              Mark as Completed
+            <p>Status: {course.enrollmentStatus}</p>
+          </li>
+        ))}
+      </ul>
+
+      <h2>All Courses</h2>
+      <ul>
+        {allCourses.map((course) => (
+          <li key={course.id}>
+            <h3>{course.name}</h3>
+            <p>Instructor: {course.instructor}</p>
+            <p>Status: {course.enrollmentStatus}</p>
+            <button onClick={() => enrollInCourse(course.id)} disabled={course.enrollmentStatus === 'Enrolled'}>
+              Enroll
             </button>
           </li>
         ))}
