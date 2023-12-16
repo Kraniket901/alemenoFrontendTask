9193492ff26b6f2d1d330aec4c0349d585faaa11
Aniket Kumar <kraniket123654@gmail.com>
2023-12-16 10:46:49 +0530

    updated courseDetails.js

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import courseModel from '../courseModel';
+import courseModel from '../courseModel'; // Update the path accordingly
 
 const CourseDetails = () => {
   const { id } = useParams();
-  const course = courseModel; // Replace with actual course data fetched from API
+  const course = courseModel.find((course) => course.id === parseInt(id, 10)); // Find the course by id
+
+  // Check if the course is not found
+  if (!course) {
+    return (
+      <div>
+        <h1>Course Not Found</h1>
+        <Link to="/">Back to Course List</Link>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1>Course Details Page</h1>
       <h2>{course.name}</h2>
       <p>Instructor: {course.instructor}</p>
-      {/* Display other course details based on the assignment */}
+      <p>Description: {course.description}</p>
+      <p>Enrollment Status: {course.enrollmentStatus}</p>
+      <p>Duration: {course.duration}</p>
+      <p>Schedule: {course.schedule}</p>
+      <p>Location: {course.location}</p>
+      <p>Prerequisites: {course.prerequisites.join(', ')}</p>
+      <h3>Syllabus:</h3>
+      <ul>
+        {course.syllabus.map((week) => (
+          <li key={week.week}>
+            <strong>Week {week.week}:</strong> {week.topic} - {week.content}
+          </li>
+        ))}
+      </ul>
+      <h3>Students:</h3>
+      <ul>
+        {course.students.map((student) => (
+          <li key={student.id}>
+            {student.name} - {student.email}
+          </li>
+        ))}
+      </ul>
       <Link to="/">Back to Course List</Link>
     </div>
   );
--- src/courseModel.js
+++ src/courseModel.js
@@ -1,40 +1,77 @@
-const courseModel = [{
-  id: 1,
-  name: 'Introduction to React Native',
-  instructor: 'John Doe',
-  description: 'Learn the basics of React Native development and build your first mobile app.',
-  enrollmentStatus: 'Open',
-  thumbnail: 'your.image.here', // Link to the course thumbnail
-  duration: '8 weeks',
-  schedule: 'Tuesdays and Thursdays, 6:00 PM - 8:00 PM',
-  location: 'Online',
-  prerequisites: ['Basic JavaScript knowledge', 'Familiarity with React'],
-  syllabus: [
-    {
-      week: 1,
-      topic: 'Introduction to React Native',
-      content: 'Overview of React Native, setting up your development environment.',
-    },
-    {
-      week: 2,
-      topic: 'Building Your First App',
-      content: 'Creating a simple mobile app using React Native components.',
-    },
-    // Additional weeks and topics...
-  ],
-  students: [
-    {
-      id: 101,
-      name: 'Alice Johnson',
-      email: 'alice@example.com',
-    },
-    {
-      id: 102,
-      name: 'Bob Smith',
-      email: 'bob@example.com',
-    },
-    // Additional enrolled students...
-  ],
-}]
+const courseModel = [
+  {
+    id: 1,
+    name: 'Introduction to React Native',
+    instructor: 'John Doe',
+    description: 'Learn the basics of React Native development and build your first mobile app.',
+    enrollmentStatus: 'Open',
+    thumbnail: 'your.image.here', // Link to the course thumbnail
+    duration: '8 weeks',
+    schedule: 'Tuesdays and Thursdays, 6:00 PM - 8:00 PM',
+    location: 'Online',
+    prerequisites: ['Basic JavaScript knowledge', 'Familiarity with React'],
+    syllabus: [
+      {
+        week: 1,
+        topic: 'Introduction to React Native',
+        content: 'Overview of React Native, setting up your development environment.',
+      },
+      {
+        week: 2,
+        topic: 'Building Your First App',
+        content: 'Creating a simple mobile app using React Native components.',
+      },
+      // Additional weeks and topics...
+    ],
+    students: [
+      {
+        id: 101,
+        name: 'Alice Johnson',
+        email: 'alice@example.com',
+      },
+      {
+        id: 102,
+        name: 'Bob Smith',
+        email: 'bob@example.com',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Advanced JavaScript',
+    instructor: 'Jane Doe',
+    description: 'Deep dive into advanced JavaScript concepts.',
+    enrollmentStatus: 'Open',
+    thumbnail: 'your.image.here',
+    duration: '6 weeks',
+    schedule: 'Mondays and Wednesdays, 7:00 PM - 9:00 PM',
+    location: 'Online',
+    prerequisites: ['Intermediate JavaScript knowledge'],
+    syllabus: [
+      {
+        week: 1,
+        topic: 'Closures and Scope',
+        content: 'Understanding closures and lexical scope.',
+      },
+      {
+        week: 2,
+        topic: 'Asynchronous JavaScript',
+        content: 'Working with Promises and Async/Await.',
+      },
+    ],
+    students: [
+      {
+        id: 103,
+        name: 'Charlie Brown',
+        email: 'charlie@example.com',
+      },
+      {
+        id: 104,
+        name: 'Diana Ross',
+        email: 'diana@example.com',
+      },
+    ],
+  },
+];
 
 export default courseModel;
